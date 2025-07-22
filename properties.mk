ee57782f944dc240d207f03a9978d1826c2a4ec7
causeri3 <vanessaca@web.de>
2025-07-22 19:55:49 +0200

    initial commit

---
+++ properties.mk
@@ -0,0 +1,4 @@
+DEVICE = descentmk2s
+SDK_HOME = ~/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-7.4.3-2024-12-11-90ec25e45
+PRIVATE_KEY = /Users/your_username/garmin_developer_key
+APP_ID = xxx
