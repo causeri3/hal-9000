93fdb4d0cc09d361cdd8a3e2fb244eaf5f338d69
causeri3 <vanessaca@web.de>
2026-01-27 14:33:57 +0100

    update sdk

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
 DEVICE = descentmk2s
-SDK_HOME = ~/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-7.4.3-2024-12-11-90ec25e45
+SDK_HOME = ~/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-8.4.0-2025-12-03-5122605dc
 PRIVATE_KEY = /Users/your_username/garmin_developer_key
-APP_ID = xxx
+APP_ID = 46e9c768-4eb1-470c-93a8-29dd11219b61
