1c05167528dba6efe8c37e2ab882b7f7aa08c0d1
causeri3 <vanessaca@web.de>
2026-04-30 22:15:12 +0300

    fix path

--- python-helpers/copy_fonts_over.py
+++ python-helpers/copy_fonts_over.py
@@ -2,21 +2,27 @@ from pathlib import Path
 import shutil
 import os
 
-# Works in IDE
 try:
-    BASE_DIR = Path(__file__).resolve().parent
+    SCRIPT_DIR = Path(__file__).resolve().parent
 except NameError:
-    BASE_DIR = Path(os.getcwd())
+    SCRIPT_DIR = Path(os.getcwd())
+
+BASE_DIR = SCRIPT_DIR.parent
 
 font_sizes = [17, 17, 19, 21, 22, 29, 31, 33, 36]
 sizes = [208, 218, 240, 260, 280, 360, 390, 416, 454]
 
-src_base = BASE_DIR / "output_fonts"
-dest_base = BASE_DIR / "resources-round"
+src_base = SCRIPT_DIR / "output_fonts"
 
 for font_size, size in zip(font_sizes, sizes):
     src = src_base / str(font_size)
-    dest = dest_base / f"{size}x{size}" / "fonts"
+
+    if size == 240:
+        base_name = f"resources-rectangle-{size}x{size}"
+    else:
+        base_name = f"resources-round-{size}x{size}"
+
+    dest = BASE_DIR / base_name / "fonts"
 
     print(f"Copying {src} -> {dest}")
 
